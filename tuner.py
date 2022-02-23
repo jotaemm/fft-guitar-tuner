0ec0a7059dff8d9d7113757a8b9d6d89f2ffcf12
jotaemm <jorgemonzonmonedero@gmail.com>
2022-02-23 19:05:51 +0000

    cosas

--- tuner.py
+++ tuner.py
@@ -1,1 +1,22 @@
+#importing modules
+from scipy.fft import fft
+import serial, time
+import numpy as np
 
+# To open a serial port to comunicate with Arduino Uno
+print("Opening serial port in COM3...")
+arduino_uno = serial.Serial(port='COM3',baudrate=9600, timeout=0.2)
+while True:
+    option = input("Enter '1' to request signal: ")
+    if option=='1':
+        break
+arduino_uno.write(('1').encode('utf-8'))
+print("\nSignal requested succesfully!\n")
+print("Reading line")
+rawString = arduino_uno.readline()
+print(rawString)
+print("Closing port...")
+arduino_uno.close()
+# To request Arduino the acoustic signal
+
+# To get the fundamental frequency of the signal
